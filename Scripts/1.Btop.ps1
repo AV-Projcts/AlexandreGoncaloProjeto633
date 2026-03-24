9bf30aa0f4e4e478b0c691d90ce1a824cb513ea4
Alex <alexpvalverde27@gmail.com>
2026-03-24 03:23:18 -0700

    Popular a vila + Primeiros scripts

---
+++ Scripts/1.Btop.ps1
@@ -0,0 +1,24 @@
+﻿# 1. btop
+cls
+if (Get-Command btop -ErrorAction SilentlyContinue) {btop}
+else {
+    Write-Host "! O programa não está instalado" -ForegroundColor Red
+    do {
+        $install = Read-Host "Instalar?"
+        switch -Regex ($install) {
+            "s|sim" {
+                winget install btop
+                if (Get-Command btop -ErrorAction SilentlyContinue) {
+                    Write-Host "\n! O programa foi instalado, iniciará em 5 segundos.." -ForegroundColor Green
+                    Start-Sleep -Seconds 5
+                    btop
+                    return
+                    }
+                else {Write-Host "! Erro, o BTOP não foi instalado, a sair.." -ForegroundColor Red}
+
+                }
+            "n|nao|não" {Write-Host "O programa não será instalado, back to the lobby.."; Return}
+            default {Write-Host "! Erro, insere Sim ou Não"}
+        }
+    }while ($true)
+}
